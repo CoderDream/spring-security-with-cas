b2fa64b045044a984bd54753407ebfa717281e18
CoderDream <coderdream@gmail.com>
2020-07-28 09:28:18 +0800

    add Spring Security Module

--- Lesson02/spring-security-demo/src/main/java/com/coderdream/controller/UserController.java
+++ Lesson02/spring-security-demo/src/main/java/com/coderdream/controller/UserController.java
@@ -2,6 +2,7 @@ package com.coderdream.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.filter.DelegatingFilterProxy;
 
 @Controller
 @RequestMapping(value = "/admin/user")
@@ -13,6 +14,7 @@ public class UserController {
      */
     @RequestMapping(value = "/list")
     public String list(){
+        DelegatingFilterProxy d = null;
         return "user_list";
     }
 }
