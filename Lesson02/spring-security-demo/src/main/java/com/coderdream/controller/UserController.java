fb03fd34dc817a7d19dee7545e85feec7ec1ae5c
CoderDream <coderdream@gmail.com>
2020-07-28 11:25:18 +0800

    数据库方式，密码加密 20200728_1125

--- Lesson02/spring-security-demo/src/main/java/com/coderdream/controller/UserController.java
+++ Lesson02/spring-security-demo/src/main/java/com/coderdream/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.coderdream.controller;
 
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.filter.DelegatingFilterProxy;
@@ -17,4 +19,11 @@ public class UserController {
         DelegatingFilterProxy d = null;
         return "user_list";
     }
+
+    public static void main(String[] args) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        System.out.println(encoder.encode("1234567"));
+        // $2a$10$8dszjSS3l0r.pdzlZcH5y.MZzunCf8BBDZqFW5mRbE72KV/coGXp2
+    }
+
 }
